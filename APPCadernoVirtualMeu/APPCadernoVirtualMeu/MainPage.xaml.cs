cb9d5097d6b473bcd861cbbc08da0326758dc731
Usuario <Usuario@CPD03-M18>
2022-05-03 12:24:58 -0300

    Adicionar arquivos de projeto.

---
+++ APPCadernoVirtualMeu/APPCadernoVirtualMeu/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using APPCadernoVirtualMeu.Terceiro;
+using APPCadernoVirtualMeu.Segundo;
+using APPCadernoVirtualMeu.Primeiro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace APPCadernoVirtualMeu
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Portugues());
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Matematica());
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Geografia());
+        }
+    }
+}
